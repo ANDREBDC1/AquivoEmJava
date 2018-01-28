417f1d0205fccbf5afa7ec24153cbc74584b465b
andre <rianbdc1@gmail.com>
2018-01-28 03:29:13 -0200

    Criação da classe de teste, implementação da classe Arquivo

--- src/teste/TesteClases.java
+++ src/teste/TesteClases.java
@@ -5,14 +5,6 @@
  */
 package teste;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
 /**
  *
  * @author Andre
@@ -21,35 +13,6 @@ public class TesteClases {
 
     public static void main(String[] args) {
         arquivo.Arquivos.compiarArquivo("C:\\Origem","C:\\Destino");
-    }
-
-    private void metoTeste() throws FileNotFoundException, IOException {
-
-        File src = new File(".");
-        String dstPath = "C:\\java\\";
-        File dst;
-
-        File[] files = src.listFiles();
-
-        for (File f : files) {
-            String fileName = f.getName();
-
-            if (fileName.contains("File")) {
-
-                dst = new File(dstPath + fileName);
-                InputStream in = new FileInputStream(f);
-                OutputStream out = new FileOutputStream(dst);
-                byte[] buf = new byte[1024];
-                int len;
-                while ((len = in.read(buf)) > 0) {
-                    out.write(buf, 0, len);
-                }
-                in.close();
-                out.close();
-                f.delete();
-            }
-
-        }
     }
 
 }
